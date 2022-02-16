7fe2e96666be284e197ba4805a821ff0a22b42f2
IsmaelBena <Ismael.benadjal@gmail.com>
2022-02-16 13:42:59 +0000

    .

---
+++ src/all-projects/AllProjects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AllProjects.css';
+
+function AllProjects() {
+  return (
+    <div className="AllProjects">
+
+    </div>
+  );
+}
+
+export default AllProjects;
